aeec3a4b024b976c7d0f51fde6268e81e7a8b21f
Luna-bf <lburattifayolle@gmail.com>
2026-01-22 13:18:33 +0100

    Ajout et modification de commentaires

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,36 +1,40 @@
+
 let darkMode = localStorage.getItem('darkMode');
 const toggleMode = document.getElementById('change-theme-btn');
 
 // Activation du mode sombre
 function enableDarkMode() {
 
-    document.body.classList.add("dark-mode"); // Je donne la classe 'dark-mode' à la balise body
+    // Ajout de la classe 'dark-mode' à la balise body
+    document.body.classList.add("dark-mode");
 
-    // Puis je met à jour l'état du mode sombre dans le localStorage
-    window.localStorage.setItem("darkMode", 'active'); //Je ne peux mettre que des strings dans le localStorage, au lieu de mettre un boléen (true ou false) je met juste 'active'
+    // Mise à jour de la valeur de la clé "darkMode" dans le localStorage
+    window.localStorage.setItem("darkMode", 'active');
 };
 
-// Désactive le mode sombre
+// Désactivation du mode sombre
 function disableDarkMode() {
 
-    document.body.classList.remove("dark-mode"); // Je retire la classe 'dark-mode' de la balise body
-    window.localStorage.setItem("darkMode", null); // Puis je change l'état du dark-mode à null
+    document.body.classList.remove("dark-mode"); // Suppression de la classe 'dark-mode' de la balise body
+    window.localStorage.setItem("darkMode", null); // Mise à jour de la valeur de la clé "darkMode" dans le localStorage
 }
 
 // Si l'utilisateur a déjà visité le site et a activé le mode sombre
 if (darkMode === 'active') {
-    enableDarkMode(); // J'appelle la fonction enableDarkMode();
+    enableDarkMode(); // J'appelle la fonction enableDarkMode()
+} else {
+    disableDarkMode(); // Sinon, j'appelle la fonction disableDarkMode()
 }
 
-// J'ajoute un événement de click au bouton qui change le thème de la page
+// Ajout d'un écouteur d'événement au bouton qui change le thème de la page
 toggleMode.addEventListener('click', function () {
 
-    // Je récupère l'élément darkMode stocké dans localStorage
+    // Récupération de la clé "darkMode" stocké dans localStorage
     darkMode = localStorage.getItem('darkMode');
 
-    // Puis je vérifie sa valeur
+    // Vérification de sa valeur
     if (darkMode !== 'active') {
-        enableDarkMode(); // Si la valeur n'est pas 'active' (thème lumineux) je donne la possibilité de l'activer
+        enableDarkMode(); // Si la valeur n'est pas 'active', je donne la possibilité d'activer le mode sombre
     } else {
         disableDarkMode(); // Sinon, je donne la possibilité de le désactiver
     }
